0e356862547bfef5a6e66c69d1f6c503e78008c2
Jeff Irwin <jeff.irwin@live.com>
2017-08-15 17:10:04 -0500

    initial routes setup

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const mustacheExpress = require('mustache-express');
 
 const app = express();
+
+app.use(express.static('public'));
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.use(require('./routes/general'));
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
